585bfc6a22d03b152230846dc03e63f05d289a2d
Azathoth <imperialbenjie393@gmail.com>
2026-05-02 16:09:25 +0800

    fix: index value

--- src/components/intellichat/organisms/IntelliChatWidget.tsx
+++ src/components/intellichat/organisms/IntelliChatWidget.tsx
@@ -33,8 +33,8 @@ export function IntelliChatWidget() {
     return (
         <div className="fixed bottom-0 left-0 right-0 sm:left-auto z-40 flex flex-col items-end pointer-events-none sm:p-6 sm:pb-[24px] sm:pr-[30px] lg:pb-[24px] lg:pr-[40px]">
             <div
-                className={`fixed sm:relative top-20 bottom-6 left-4 right-4 sm:top-auto sm:bottom-auto sm:left-auto sm:right-auto sm:w-auto transition-all duration-300 transform origin-bottom sm:origin-bottom-right pointer-events-auto flex flex-col justify-end ${isOpen
-                    ? 'opacity-100 translate-y-0 scale-100'
+                className={`fixed sm:relative top-20 bottom-6 left-4 right-4 sm:top-auto sm:bottom-auto sm:left-auto sm:right-auto sm:w-auto transition-all duration-300 transform origin-bottom sm:origin-bottom-right flex flex-col justify-end ${isOpen
+                    ? 'opacity-100 translate-y-0 scale-100 pointer-events-auto'
                     : 'opacity-0 translate-y-8 scale-95 pointer-events-none'
                     }`}
             >
@@ -42,7 +42,7 @@ export function IntelliChatWidget() {
             </div>
 
             <div
-                className={`fixed bottom-6 right-6 pointer-events-auto transition-all duration-300 ${isOpen ? 'opacity-0 scale-75 pointer-events-none' : 'opacity-100 scale-100'
+                className={`fixed bottom-6 right-6 transition-all duration-300 ${isOpen ? 'opacity-0 scale-75 pointer-events-none' : 'opacity-100 scale-100 pointer-events-auto'
                     }`}
             >
                 <ChatbotToggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
